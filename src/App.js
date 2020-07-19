dd2341ef96a58c951710980bff091c90cdc0c796
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-07-19 18:28:40 +0530

    Seventh Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home'
 import Apod from './components/Apod';
 import Neows from './components/Neows';
-const App = () => {
-  return (
-    <div>
-      <div className="container">
-        <BrowserRouter basename="https://vinaymodepalli.github.io/nasa/">
-        <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path = "/apod" component={Apod} />
-            <Route path = "/neows" component={Neows} />
-          </Switch>
-        </BrowserRouter>
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <div className="container">
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
+          
+          <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path = "/apod" component={Apod} />
+              <Route path = "/neows" component={Neows} />
+            </Switch>
+          </BrowserRouter>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
