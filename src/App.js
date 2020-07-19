395ad4bbf0bae2b624841bcb8da5b643de664511
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-07-19 17:56:40 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <div className="container">
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
