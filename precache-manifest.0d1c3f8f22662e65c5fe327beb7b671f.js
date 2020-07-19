041702ceabd80aa24a622a281c3d348dc03b8806
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-07-19 17:08:18 +0530

    Updates

---
+++ precache-manifest.0d1c3f8f22662e65c5fe327beb7b671f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f1beecaa0d6c86b72df2fbc55c3c43",
+    "url": "/nasa/index.html"
+  },
+  {
+    "revision": "212fc461c06c5f828365",
+    "url": "/nasa/static/css/main.34811919.chunk.css"
+  },
+  {
+    "revision": "ed2c801cf7582249c29a",
+    "url": "/nasa/static/js/2.8522ad16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa/static/js/2.8522ad16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212fc461c06c5f828365",
+    "url": "/nasa/static/js/main.47e2a359.chunk.js"
+  },
+  {
+    "revision": "649df1e927f3ecb3dd5c",
+    "url": "/nasa/static/js/runtime-main.30ff7636.js"
+  }
+]);
